eed4aa49c8787085ac06f017e205b99a1ad23bcb
Itay Ostraich <itayost1@gmail.com>
2025-11-16 12:19:31 +0200

    Update VideoShowcase.tsx

--- src/components/sections/VideoShowcase.tsx
+++ src/components/sections/VideoShowcase.tsx
@@ -35,7 +35,7 @@ const VideoShowcase = () => {
             <div className="relative w-full" style={{ paddingBottom: '177.78%' /* 16:9 inverted for vertical */ }}>
               <iframe
                 className="absolute top-0 left-0 w-full h-full"
-                src="https://www.youtube.com/embed/WG2o_WcKZ6g"
+                src="https://www.youtube.com/embed/WG2o_WcKZ6g?autoplay=1&mute=1&loop=1&playlist=WG2o_WcKZ6g"
                 title="סרטון הצגה"
                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
                 allowFullScreen
